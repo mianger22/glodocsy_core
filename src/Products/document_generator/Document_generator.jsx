995f994d1cd7507e510ea3a818985e2c8f2139ec
mianger22 <test666777888999@yandex.ru>
2024-08-15 10:22:58 +0300

    Added another field

--- src/Products/document_generator/Document_generator.jsx
+++ src/Products/document_generator/Document_generator.jsx
@@ -2,8 +2,10 @@ import { Formik } from 'formik';
 
 const Document_generator = () => (
   <div>
+    <div className="form-title text-indigo-600 text-sm mt-4 mb-4"><ins>Задание</ins></div>
+                
     <Formik
-      initialValues={{ val1: '', val2: '' }}
+      initialValues={{ Patrol_task_number: '', val2: '' }}
       validate={values => {
         const errors = {};
         if (!values.email) {
@@ -33,35 +35,43 @@ const Document_generator = () => (
         /* and other goodies */
       }) => (
         <form onSubmit={handleSubmit}>
-            <div className="uk-margin">
-                <input 
-                    className="uk-input" 
-                    type="text" 
-                    placeholder="val1" 
-                    aria-label="Input"
-                    name="val1"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.val1}
-                />
+            <div classNameName="mb-4">
+                <label classNameName="input-label" for="Patrol_task_number" style={{color: "rgb(167, 150, 0);"}}>Номер задания</label>
+
+                <div classNameName="uk-margin">
+                    <input 
+                        classNameName="uk-input" 
+                        type="number" 
+                        placeholder="185" 
+                        aria-label="Patrol_task_number"
+                        name="Patrol_task_number"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.Patrol_task_number}
+                    />
+                </div>
+                {errors.Patrol_task_number && touched.Patrol_task_number && errors.Patrol_task_number}
             </div>
-            {errors.val1 && touched.val1 && errors.val1}
-            <div className="uk-margin">
-                <input 
-                    className="uk-input" 
-                    type="text" 
-                    placeholder="val2" 
-                    aria-label="val2"
-                    name="val2"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.val2}
-                />
+            <div className="mb-4">
+                <label className="input-label" for="Date_issue_task">От какого числа</label>
+
+                <div classNameName="uk-margin">
+                    <input 
+                        classNameName="uk-input date_selection" 
+                        type="text" 
+                        placeholder="«10» июня 2024" 
+                        aria-label="Date_issue_task"
+                        name="Date_issue_task"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.Date_issue_task}
+                    />
+                </div>
+                {errors.Date_issue_task && touched.Date_issue_task && errors.Date_issue_task}
             </div>
-            {errors.val2 && touched.val2 && errors.val2}
             
             <button type="submit" disabled={isSubmitting}>
-                Submit
+                Создать документы
             </button>
         </form>
       )}
