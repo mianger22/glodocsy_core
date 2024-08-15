5f071cddfd21863f0b0da3bdacfbac10fc843d9b
mianger22 <test666777888999@yandex.ru>
2024-08-15 07:55:23 +0300

    added method click

--- src/PartsAppFile/CenterBlock/CardLink.jsx
+++ src/PartsAppFile/CenterBlock/CardLink.jsx
@@ -1,6 +1,10 @@
 function CardLink({title_card}) {  
+    const clicked_card = () => {
+        alert('Clicked')
+    }
+
     return (
-        <div class="uk-margin uk-card uk-card-primary uk-card-body">
+        <div class="uk-margin uk-card uk-card-primary uk-card-body" onClick={clicked_card}>
             <h3 class="uk-card-title">{title_card}</h3>
         </div>
     );
