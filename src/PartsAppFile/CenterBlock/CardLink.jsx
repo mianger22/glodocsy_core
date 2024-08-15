c20cbd12ef1d810498ce166549ed2658f42e497a
mianger22 <test666777888999@yandex.ru>
2024-08-15 07:53:47 +0300

    ап

---
+++ src/PartsAppFile/CenterBlock/CardLink.jsx
@@ -0,0 +1,9 @@
+function CardLink({title_card}) {  
+    return (
+        <div class="uk-margin uk-card uk-card-primary uk-card-body">
+            <h3 class="uk-card-title">{title_card}</h3>
+        </div>
+    );
+}
+
+export default CardLink;
--- src/PartsAppFile/CenterBlock/CenterBlock.jsx
+++ src/PartsAppFile/CenterBlock/CenterBlock.jsx
@@ -1,16 +1,10 @@
+import CardLink from "./CardLink";
+
 function CenterBlock() {  
     return (
         <div>
-            <div>
-                <div class="uk-margin uk-card uk-card-primary uk-card-body">
-                    <h3 class="uk-card-title">Отчёт по патрулированию</h3>
-                </div>
-            </div>
-            <div>
-                <div class="uk-margin uk-card uk-card-primary uk-card-body">
-                    <h3 class="uk-card-title">Подсчёт кубатуры</h3>
-                </div>
-            </div>
+            <CardLink title_card="Отчёт по патрулированию" />
+            <CardLink title_card="Подсчёт кубатуры" />
         </div>
     );
 }
