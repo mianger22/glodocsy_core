e480f82c231e8fca742f1cd4da108f857e8c97e5
mianger22 <test666777888999@yandex.ru>
2024-08-15 10:11:10 +0300

    added formik

--- src/PartsAppFile/CenterBlock/CenterBlock.jsx
+++ src/PartsAppFile/CenterBlock/CenterBlock.jsx
@@ -1,10 +1,12 @@
+import Document_generator from "../../Products/document_generator/Document_generator";
 import CardLink from "./CardLink";
 
 function CenterBlock() {  
     return (
         <div>
-            <CardLink title_card="Отчёт по патрулированию" />
-            <CardLink title_card="Подсчёт кубатуры" />
+            {/* <CardLink title_card="Отчёт по патрулированию" /> */}
+            {/* <CardLink title_card="Подсчёт кубатуры" /> */}
+            <Document_generator />
         </div>
     );
 }
--- src/Products/document_generator/Document_generator.jsx
+++ src/Products/document_generator/Document_generator.jsx
@@ -1,9 +1,72 @@
-function Document_generator() {
-  return (
-    <div>
-    
-    </div>
-  );
-}
+import { Formik } from 'formik';
+
+const Document_generator = () => (
+  <div>
+    <Formik
+      initialValues={{ val1: '', val2: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+            <div className="uk-margin">
+                <input 
+                    className="uk-input" 
+                    type="text" 
+                    placeholder="val1" 
+                    aria-label="Input"
+                    name="val1"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.val1}
+                />
+            </div>
+            {errors.val1 && touched.val1 && errors.val1}
+            <div className="uk-margin">
+                <input 
+                    className="uk-input" 
+                    type="text" 
+                    placeholder="val2" 
+                    aria-label="val2"
+                    name="val2"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.val2}
+                />
+            </div>
+            {errors.val2 && touched.val2 && errors.val2}
+            
+            <button type="submit" disabled={isSubmitting}>
+                Submit
+            </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default Document_generator;
