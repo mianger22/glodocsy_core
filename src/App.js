87fe9728fc9f252d0e1eefd0e19fba8fd269f330
mianger22 <test666777888999@yandex.ru>
2024-08-17 08:42:13 +0300

    Deleted the extra blocks

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import CenterBlock from "./PartsAppFile/CenterBlock/CenterBlock";
 function App() {
   return (
     <div className="uk-grid-match uk-child-width-expand@s uk-flex" uk-grid>
-      <div>
-        <div className="uk-card uk-card-body"></div>
+      <div className="uk-card uk-card-body"></div>
+      <div className="uk-card uk-card-body">
+        <CenterBlock />
       </div>
-      <div>
-        <div className="uk-card uk-card-body">
-          <CenterBlock />
-        </div>
-      </div>
-      <div>
-        <div className="uk-card uk-card-body"></div>
-      </div>
+      <div className="uk-card uk-card-body"></div>
   </div>
   );
 }
