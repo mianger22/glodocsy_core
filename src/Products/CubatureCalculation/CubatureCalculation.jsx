a76e9bb4d7cc3bd7d16903f40d13deff518d8261
mianger22 <test666777888999@yandex.ru>
2024-08-31 11:33:27 +0300

    ап

--- src/Products/CubatureCalculation/CubatureCalculation.jsx
+++ src/Products/CubatureCalculation/CubatureCalculation.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const CubatureCalculation = () => {
+    const [isEl, setEl] = useState(0);
+
+    const change_el_val = (val) => {
+        setEl(prevIsEl => prevIsEl + val);
+    }
+
     return (
         <div className='uk-margin-small-bottom'>
             <h1>Подсчёт кубатуры</h1>
+
+            <h5>Ель</h5>
+            <div className="uk-flex">
+                <div className="uk-margin-small-right">0,029</div>
+                <div className="uk-margin-small-right">8</div>
+                <div className="uk-margin-small-right">
+                    <button onClick={() => change_el_val(0.029)}>+</button>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                итог: {isEl}
+            </div>
         </div>
     )
 }
