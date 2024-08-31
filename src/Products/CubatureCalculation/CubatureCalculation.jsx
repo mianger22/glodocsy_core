3c10788e8ec289c71ed94c250db34120a10c03fa
mianger22 <test666777888999@yandex.ru>
2024-08-31 12:16:28 +0300

    upgraded visual

--- src/Products/CubatureCalculation/CubatureCalculation.jsx
+++ src/Products/CubatureCalculation/CubatureCalculation.jsx
@@ -20,37 +20,38 @@ const CubatureCalculation = () => {
         <div className='uk-margin-small-bottom'>
             <h1>Подсчёт кубатуры</h1>
 
-            <h5>Ель</h5>
-            
+            <div className="uk-inline uk-margin-small-bottom">
+                <button className="uk-button uk-button-default" type="button" uk-toggle="target: #dropdown-1">Ель - 4</button>
+                <div id="dropdown-1" uk-dropdown="mode: click">
+                    <ul className="uk-nav uk-dropdown-nav">
+                        <CubatureList diameter={8} volume={0.029} handlerClick={change_el_val} />
+                        <CubatureList diameter={12} volume={0.091} handlerClick={change_el_val} />
+                        <CubatureList diameter={16} volume={0.19} handlerClick={change_el_val} />
+                        <CubatureList diameter={20} volume={0.33} handlerClick={change_el_val} />
+                        <CubatureList diameter={24} volume={0.50} handlerClick={change_el_val} />
+                        <CubatureList diameter={28} volume={0.71} handlerClick={change_el_val} />
+                        <CubatureList diameter={32} volume={0.96} handlerClick={change_el_val} />
+                        <CubatureList diameter={36} volume={1.25} handlerClick={change_el_val} />
+                        <CubatureList diameter={40} volume={1.74} handlerClick={change_el_val} />
+                        <CubatureList diameter={44} volume={1.91} handlerClick={change_el_val} />
+                        <CubatureList diameter={48} volume={2.31} handlerClick={change_el_val} />
+                        <CubatureList diameter={52} volume={2.74} handlerClick={change_el_val} />
+                        <CubatureList diameter={56} volume={3.19} handlerClick={change_el_val} />
+                    </ul>
+                </div>
+            </div>
+
             <div>
-                {inf}
-                итог: {isEl}
-                всего: {isKol}
+                <div className='uk-text-danger'>
+                    {inf}
+                </div>
+                <div>
+                    Итог: <span className="uk-badge">{isEl}</span>
+                </div>
+                <div>
+                    Всего: <span className="uk-badge">{isKol}</span>
+                </div>
             </div>
-
-            <div class="uk-inline">
-
-            <button class="uk-button uk-button-default" type="button" uk-toggle="target: #dropdown-1">Ель - 4</button>
-            <div id="dropdown-1" uk-dropdown="mode: click">
-                <ul class="uk-nav uk-dropdown-nav">
-                    <CubatureList diameter={8} volume={0.029} handlerClick={change_el_val} />
-                    <CubatureList diameter={12} volume={0.091} handlerClick={change_el_val} />
-                    <CubatureList diameter={16} volume={0.19} handlerClick={change_el_val} />
-                    <CubatureList diameter={20} volume={0.33} handlerClick={change_el_val} />
-                    <CubatureList diameter={24} volume={0.50} handlerClick={change_el_val} />
-                    <CubatureList diameter={28} volume={0.71} handlerClick={change_el_val} />
-                    <CubatureList diameter={32} volume={0.96} handlerClick={change_el_val} />
-                    <CubatureList diameter={36} volume={1.25} handlerClick={change_el_val} />
-                    <CubatureList diameter={40} volume={1.74} handlerClick={change_el_val} />
-                    <CubatureList diameter={44} volume={1.91} handlerClick={change_el_val} />
-                    <CubatureList diameter={48} volume={2.31} handlerClick={change_el_val} />
-                    <CubatureList diameter={52} volume={2.74} handlerClick={change_el_val} />
-                    <CubatureList diameter={56} volume={3.19} handlerClick={change_el_val} />
-                </ul>
-            </div>
-        </div>
-
-
         </div>
     )
 }
