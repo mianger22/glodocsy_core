b195870831df7679cf30025388bc03c74a940750
mianger22 <test666777888999@yandex.ru>
2024-08-31 12:58:31 +0300

    up

--- src/Products/CubatureCalculation/CubatureCalculation.jsx
+++ src/Products/CubatureCalculation/CubatureCalculation.jsx
@@ -6,16 +6,6 @@ const CubatureCalculation = () => {
     const [isKol, setKol] = useState(0);
     const [inf, setInf] = useState('');
 
-    const change_el_val = (val) => {
-        setEl(prevIsEl => prevIsEl + val);
-        setKol(prevIsKol => prevIsKol + 1);
-
-        setInf('+ 1');
-        setTimeout(()=>{
-            setInf('');
-        }, 1000);
-    }
-
     const cubatureData = [
         {
             id: 1,
@@ -36,14 +26,43 @@ const CubatureCalculation = () => {
                 {id: 12, diameter: 52, volume: 2.74},
                 {id: 13, diameter: 56, volume: 3.19},
             ]
+        },
+        {
+            id: 2,
+            breed: 'Сосна',
+            category: 4,
+            cubatures: [
+                {id: 1, diameter: 8, volume: 0.037},
+                {id: 2, diameter: 12, volume: 0.098},
+                {id: 3, diameter: 16, volume: 0.19},
+                {id: 4, diameter: 20, volume: 0.32},
+                {id: 5, diameter: 24, volume: 0.48},
+                {id: 6, diameter: 28, volume: 0.68},
+                {id: 7, diameter: 32, volume: 0.90},
+                {id: 8, diameter: 36, volume: 1.16},
+                {id: 9, diameter: 40, volume: 1.45},
+                {id: 10, diameter: 44, volume: 1.77},
+                {id: 11, diameter: 48, volume: 2.14},
+            ]
         }
     ]
+
+    const change_el_val = (val) => {
+        setEl(prevIsEl => prevIsEl + val);
+        setKol(prevIsKol => prevIsKol + 1);
+
+        setInf('+ 1');
+        setTimeout(()=>{
+            setInf('');
+        }, 1000);
+    }
 
     return (
         <div>
             <h1>Подсчёт кубатуры</h1>
 
-            <CubatureCalculationBlock handlerClick={change_el_val} cubatureData={cubatureData[0]} />
+            <CubatureCalculationBlock handlerClick={change_el_val} cubatureData={cubatureData[0]} cubatureDataId={1} />
+            <CubatureCalculationBlock handlerClick={change_el_val} cubatureData={cubatureData[1]} cubatureDataId={2} />
 
             <div>
                 <div className='uk-text-danger'>
--- src/Products/CubatureCalculation/CubatureCalculationBlock.jsx
+++ src/Products/CubatureCalculation/CubatureCalculationBlock.jsx
@@ -1,11 +1,11 @@
 import CubatureList from "./CubatureList";
 
-const CubatureCalculationBlock = ({handlerClick, cubatureData}) => {
+const CubatureCalculationBlock = ({handlerClick, cubatureData, cubatureDataId}) => {
     return (
         <div className='uk-margin-small-bottom'>
             <div className="uk-inline uk-margin-small-bottom">
-                <button className="uk-button uk-button-default" type="button" uk-toggle="target: #dropdown-1">{cubatureData.breed} - {cubatureData.category}</button>
-                <div id="dropdown-1" uk-dropdown="mode: click">
+                <button className="uk-button uk-button-default" type="button" uk-toggle={`target: #dropdown-${cubatureDataId}`}>{cubatureData.breed} - {cubatureData.category}</button>
+                <div id={`dropdown-${cubatureDataId}`} uk-dropdown="mode: click">
                     <ul className="uk-nav uk-dropdown-nav">
                         {cubatureData.cubatures.map(el => 
                             <CubatureList 
