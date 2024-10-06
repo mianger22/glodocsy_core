2a5f095b4c718f45a42b40d5dc16fb8bf89a8e92
mianger22 <test666777888999@yandex.ru>
2024-10-06 08:13:28 +0300

    up

--- src/ComponentsHomePage/ContentBlock.jsx
+++ src/ComponentsHomePage/ContentBlock.jsx
@@ -52,6 +52,12 @@ function ContentBlock() {
                     ? <CountingNotExportedWood />
                     : ''}
 
+            {is_show_counting_not_exported_wood === undefined
+                ? <CardLink title_card='Инвентаризация' click_handler={show_counting_not_exported_wood} />
+                : is_show_counting_not_exported_wood === true
+                    ? <CountingNotExportedWood />
+                    : ''}
+
             {
                 (is_show_patrol_report_generator !== undefined || 
                     is_show_cubature_calculation !== undefined || 
